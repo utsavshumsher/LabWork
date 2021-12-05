c21a3bb4bcedf8f5184578341e84eac288603f0c
utsav <utsavshumsherthapa@gmail.com>
2021-12-05 10:48:13 +0545

    Day2

--- labwork.py
+++
@@ -1,8 +0,0 @@
-#sum of three numbers
-a=int(input("enter first number"))
-b=int(input("enter second number"))
-c=int(input("enter third number"))
-d=a+b+c
-print(f"the sum of the three numbers is {d}")
-
-
